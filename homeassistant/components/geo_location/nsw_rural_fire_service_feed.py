978442218652eb1dd7657f00151b68ee123ddb52
Adam Mills <adam@armills.info>
2018-10-08 23:14:02 -0400

    Restore states through a JSON store

--- homeassistant/components/geo_location/nsw_rural_fire_service_feed.py
+++ homeassistant/components/geo_location/nsw_rural_fire_service_feed.py
@@ -189,7 +189,6 @@ class NswRuralFireServiceLocationEvent(GeoLocationEvent):
     async def async_added_to_hass(self):
         """Call when entity is added to hass."""
         self._remove_signal_delete = async_dispatcher_connect(
-            self.hass, SIGNAL_DELETE_ENTITY.format(self._external_id),
             self._delete_callback)
         self._remove_signal_update = async_dispatcher_connect(
             self.hass, SIGNAL_UPDATE_ENTITY.format(self._external_id),
